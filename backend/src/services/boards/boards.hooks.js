ecc38e15ff27d9d7f0688e6235e8e0b46850a6c3
Amritpal Singh <amrit3701@gmail.com>
2022-07-12 14:10:16 +0530

    Pushed backend code

---
+++ backend/src/services/boards/boards.hooks.js
@@ -0,0 +1,49 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+
+const { setField } = require('feathers-authentication-hooks');
+
+const setUserId = setField({
+  from: 'params.user._id',
+  as: 'data.ownerId'
+});
+
+const limitToUser = setField({
+  from: 'params.user._id',
+  as: 'params.query.ownerId'
+});
+
+
+module.exports = {
+  before: {
+    all: [ authenticate('jwt') ],
+    find: [limitToUser],
+    get: [limitToUser],
+    create: [
+      setUserId
+      // hooks.associateCurrentUser({ as: 'ownerId' }),
+    ],
+    update: [limitToUser],
+    patch: [limitToUser],
+    remove: [limitToUser]
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
