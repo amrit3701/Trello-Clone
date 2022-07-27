03734f080d7407cefc7847c5f27e7cb77a569966
Amritpal Singh <amrit3701@gmail.com>
2022-07-27 22:40:42 +0530

    fronted: fixed bug not to show create dialog on /board when user is not authorised

--- frontend/src/views/Board.vue
+++ frontend/src/views/Board.vue
@@ -1,140 +1,142 @@
 <template>
   <v-container fluid>
-    <v-layout row wrap v-if="!errorInList">
-      <v-row class="ma-2 pa-2">
-        <div class="text-h4" v-if="board">{{ board.name }}</div>
-        <v-spacer></v-spacer>
-        <div>
-          <v-dialog
-            model="false"
-            width="600px"
+    <div v-if="loadingBoard || loadingList">
+      <v-progress-linear
+        indeterminate
+        color="grey"
+      ></v-progress-linear>
+    </div>
+    <div v-else>
+      <v-layout row wrap v-if="!errorInList">
+        <v-row class="ma-2 pa-2">
+          <div class="text-h4" v-if="board">{{ board.name }}</div>
+          <v-spacer></v-spacer>
+          <div>
+            <v-dialog
+              model="false"
+              width="600px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  text
+                  class="text-decoration-none"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  See Activities
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">Activities</span>
+                </v-card-title>
+              <v-card-text>
+                <v-list three-line>
+                  <template v-for="(activity, index) in activitiesByDate">
+                    <v-list-item :key="activity._id">
+                      <v-list-item-content>
+                        <v-list-item-title v-html="markdownToHtml(activity.text)">
+                        </v-list-item-title>
+                        <v-list-item-subtitle
+                          v-html="beautifyDate(activity.createdAt)"
+                        ></v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-divider :key="index"></v-divider>
+                  </template>
+                </v-list>
+              </v-card-text>
+                </v-card>
+            </v-dialog>
+          </div>
+        </v-row>
+      </v-layout>
+      <v-slide-y-transition mode="out-in">
+        <v-layout row wrap v-if="errorInList">
+          <v-col>
+            <v-alert dense outlined type="error">
+              You are not authorized access this board.
+            </v-alert>
+          </v-col>
+        </v-layout>
+        <v-layout row wrap v-if="!loadingList">
+          <v-col xl="1" lg="2" md="3" sm="4" v-for="list in lists" :key="list._id"
+              :class="{ 'cyan lighten-5': droppingList == list }"
           >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                text
-                class="text-decoration-none"
-                v-bind="attrs"
-                v-on="on"
-              >
-                See Activities
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">Activities</span>
-              </v-card-title>
-            <v-card-text>
-              <v-list three-line>
-                <template v-for="(activity, index) in activitiesByDate">
-                  <v-list-item :key="activity._id">
-                    <v-list-item-content>
-                      <v-list-item-title v-html="markdownToHtml(activity.text)"></v-list-item-title>
-                      <v-list-item-subtitle
-                        v-html="beautifyDate(activity.createdAt)"
-                      ></v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-divider :key="index"></v-divider>
-                </template>
+            <v-card
+              @dragover="setDroppingList($event, list)"
+            >
+              <v-toolbar color="cyan" dark>
+                <v-toolbar-title>{{ list.name }}</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-list one-line>
+                  <div v-for="card in cardsByListId[list._id]" :key="card._id">
+                    <v-list-item
+                      draggable="true"
+                      @dragend="dropCard(card)"
+                    >
+                      <v-list-item-content>
+                        <v-list-item-title v-html="card.title">
+                          {{ card.title }}
+                        </v-list-item-title>
+                        <v-list-item-subtitle v-html="card.description">
+                          {{ card.description }}
+                        </v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-divider
+                    ></v-divider>
+                  </div>
               </v-list>
-            </v-card-text>
-              </v-card>
-          </v-dialog>
-        </div>
-      </v-row>
-    </v-layout>
-    <v-slide-y-transition mode="out-in">
-      <v-layout row wrap v-if="errorInList">
-        <v-col>
-          <v-alert dense outlined type="error">
-            You are not authorized access this board.
-          </v-alert>
-        </v-col>
-      </v-layout>
-      <v-layout row wrap v-else-if="!errorInList">
-        <v-progress-circular
-          :size="50"
-          color="primary"
-          indeterminate
-          v-if="loadingBoard || loadingList"
-        ></v-progress-circular>
-        <v-col xl="1" lg="2" md="3" sm="4" v-for="list in lists" :key="list._id"
-            :class="{ 'cyan lighten-5': droppingList == list }"
-        >
-          <v-card
-            @dragover="setDroppingList($event, list)"
-          >
-            <v-toolbar color="cyan" dark>
-              <v-toolbar-title>{{ list.name }}</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-magnify</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-list one-line>
-              <!-- <v-subheader></v-subheader> -->
-                <div v-for="card in cardsByListId[list._id]" :key="card._id">
-                  <v-list-item
-                    draggable="true"
-                    @dragend="dropCard(card)"
-                  >
-                    <v-list-item-content>
-                      <v-list-item-title v-html="card.title">
-                        {{ card.title }}
-                      </v-list-item-title>
-                      <v-list-item-subtitle v-html="card.description">
-                        {{ card.description }}
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-divider
-                  ></v-divider>
-                </div>
-            </v-list>
-            <v-card-actions>
-              <CardComponent
-                :boardId="$route.params.id"
-                :listId="list._id"
-                :user="user"
-                :createActivity="createActivity"
-              >
-              </CardComponent>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col xl="1" lg="2" md="3" sm="4">
-          <v-card>
-            <v-card-title column style="flex-direction: column">
-              <div class="headline">Create List</div>
-              <div>
-                <v-form
-                  ref="form"
-                  v-model="valid"
-                  @submit.prevent="createList"
-                  @keydown.prevent.enter
-                  if-v="!creatingList"
-                  >
-                  <v-text-field
-                    v-model="list.name"
-                    :rules="notEmptyRules"
-                    label="name"
-                    required
-                  ></v-text-field>
-                  <v-btn type="submit" v-bind:disabled="!valid">Create</v-btn>
-                </v-form>
+              <v-card-actions>
+                <CardComponent
+                  :boardId="$route.params.id"
+                  :listId="list._id"
+                  :user="user"
+                  :createActivity="createActivity"
+                >
+                </CardComponent>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col xl="1" lg="2" md="3" sm="4">
+            <v-card>
+              <v-card-title column style="flex-direction: column">
+                <div class="headline">Create List</div>
+                <div>
+                  <v-form
+                    ref="form"
+                    v-model="valid"
+                    @submit.prevent="createList"
+                    @keydown.prevent.enter
+                    v-if="!lists.isCreatePending"
+                    >
+                    <v-text-field
+                      v-model="list.name"
+                      :rules="notEmptyRules"
+                      label="name"
+                      required
+                    ></v-text-field>
+                    <v-btn type="submit" v-bind:disabled="!valid">Create</v-btn>
+                  </v-form>
                   <v-progress-circular
-                  :size="50"
-                  color="primary"
-                  indeterminate
-                  v-if="creatingList"
+                    :size="50"
+                    color="primary"
+                    indeterminate
+                    v-if="lists.isCreatePending"
                   ></v-progress-circular>
-              </div>
-            </v-card-title>
-          </v-card>
-        </v-col>
-        <v-spacer></v-spacer>
-      </v-layout>
-    </v-slide-y-transition>
+                </div>
+              </v-card-title>
+            </v-card>
+          </v-col>
+          <v-spacer></v-spacer>
+        </v-layout>
+      </v-slide-y-transition>
+    </div>
   </v-container>
 </template>
 
@@ -154,13 +156,12 @@ export default {
     droppingList: null,
     valid: false,
     notEmptyRules: [(v) => !!v || 'Cannot be empty'],
-
   }),
   created() {
-    models.api.Board.get(this.$route.params.id);
-    this.List.find({ query: { boardId: this.$route.params.id } });
     this.list = new this.List();
 
+    models.api.Board.get(this.$route.params.id);
+    this.List.find({ query: { boardId: this.$route.params.id } });
     this.Card.find({ query: { boardId: this.$route.params.id } });
     this.Activity.find({ query: { boardId: this.$route.params.id } });
   },
