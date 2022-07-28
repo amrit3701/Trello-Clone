d67df787ff321b52061419e34bb6187411111f13
Amritpal Singh <amrit3701@gmail.com>
2022-07-28 22:36:18 +0530

    frontend: added breakpoints and fixed skeleton in Boards page

--- frontend/src/views/Boards.vue
+++ frontend/src/views/Boards.vue
@@ -1,28 +1,24 @@
 <template>
   <v-container fluid>
     <v-row dense>
-      <v-col cols="3" v-if="loading">
+      <v-col
+        xl="1" lg="2" md="3" sm="4" xs="6"
+        v-show="loading"
+        v-for="n in 2"
+        :key="n"
+      >
         <v-skeleton-loader
-          class="mx-auto"
-          max-width="500"
-          type="card"
-        ></v-skeleton-loader>
-      </v-col>
-      <v-col cols="3" v-if="loading">
-        <v-skeleton-loader
-          class="mx-auto"
-          max-width="500"
-          type="card"
+          v-bind="attrs"
+          type="image, card-heading, button"
         ></v-skeleton-loader>
       </v-col>
       <v-col
-        cols="3"
+        xl="1" lg="2" md="3" sm="4" xs="6"
         v-for="board in boards"
         :key="board._id"
       >
         <v-card
           class="mx-auto"
-          max-width="500"
         >
           <v-img
             height="250"
@@ -40,50 +36,50 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="3">
-          <v-card class="mx-auto" max-width="500">
-            <v-card-title class="text-h5">
-              <div class="headline">Create Board</div>
-            </v-card-title>
-            <v-card-text class="text--primary">
-              <div>
-                <v-form
-                  ref="form"
-                  v-model="valid"
-                  @submit.prevent="createBoard"
-                  @keydown.prevent.enter
-                  v-if="!board.isCreatePending"
-                  >
-                  <v-text-field
-                    v-model="board.name"
-                    :rules="notEmptyRules"
-                    label="name"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="board.background"
-                    :rules="notEmptyRules"
-                    label="background"
-                    required
-                  ></v-text-field>
-                  <v-btn
-                    color="primary"
-                    text
-                    type="submit"
-                    v-bind:disabled="!valid"
-                  >
-                    Create
-                  </v-btn>
-                </v-form>
-                  <v-progress-circular
-                  :size="50"
+      <v-col xl="1" lg="2" md="3" sm="4" xs="6">
+        <v-card class="mx-auto">
+          <v-card-title class="text-h5">
+            <div class="headline">Create Board</div>
+          </v-card-title>
+          <v-card-text class="text--primary">
+            <div>
+              <v-form
+                ref="form"
+                v-model="valid"
+                @submit.prevent="createBoard"
+                @keydown.prevent.enter
+                v-if="!board.isCreatePending"
+                >
+                <v-text-field
+                  v-model="board.name"
+                  :rules="notEmptyRules"
+                  label="name"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="board.background"
+                  :rules="notEmptyRules"
+                  label="background"
+                  required
+                ></v-text-field>
+                <v-btn
                   color="primary"
-                  indeterminate
-                  v-if="board.isCreatePending"
-                  ></v-progress-circular>
-              </div>
-            </v-card-text>
-          </v-card>
+                  text
+                  type="submit"
+                  v-bind:disabled="!valid"
+                >
+                  Create
+                </v-btn>
+              </v-form>
+                <v-progress-circular
+                :size="50"
+                color="primary"
+                indeterminate
+                v-if="board.isCreatePending"
+                ></v-progress-circular>
+            </div>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -99,6 +95,11 @@ export default {
   data: () => ({
     valid: false,
     notEmptyRules: [(v) => !!v || 'Cannot be empty'],
+    attrs: {
+      // class: 'mb-12',
+      // boilerplate: true,
+      elevation: 2,
+    },
   }),
   created() {
     this.board = new this.Board();
@@ -122,3 +123,11 @@ export default {
   },
 };
 </script>
+<style scoped>
+>>> .v-skeleton-loader__button {
+  margin: 10px;
+}
+>>> .v-skeleton-loader__image {
+  height: 250px;
+}
+</style>
